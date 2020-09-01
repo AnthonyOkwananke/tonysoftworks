55ae42e8d40a1a6f24e6c99ac0fe9f1226f8ee06
Anthony Okwananke <826717@ACDM.DS.SAIT.CA>
2020-08-31 20:02:35 -0600

    Updates

---
+++ precache-manifest.f4161185e495cb372f202662088be5dc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e0903650a92b8c6c4fe1772ae1e0f9",
+    "url": "/AnthonyOkwananke/tony-softworks.github.io/index.html"
+  },
+  {
+    "revision": "95e357a74af332740f9b",
+    "url": "/AnthonyOkwananke/tony-softworks.github.io/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "e1b2412c5d4e9c92258c",
+    "url": "/AnthonyOkwananke/tony-softworks.github.io/static/css/main.d33cfbfd.chunk.css"
+  },
+  {
+    "revision": "95e357a74af332740f9b",
+    "url": "/AnthonyOkwananke/tony-softworks.github.io/static/js/2.e566aad9.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/AnthonyOkwananke/tony-softworks.github.io/static/js/2.e566aad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b2412c5d4e9c92258c",
+    "url": "/AnthonyOkwananke/tony-softworks.github.io/static/js/main.d5fe7e29.chunk.js"
+  },
+  {
+    "revision": "f38520e842b48b33648b",
+    "url": "/AnthonyOkwananke/tony-softworks.github.io/static/js/runtime-main.88b7e030.js"
+  },
+  {
+    "revision": "82a318ccea22a8af9fc4590f41df9238",
+    "url": "/AnthonyOkwananke/tony-softworks.github.io/static/media/iconfinder_Reactjs_logo_1174949.82a318cc.png"
+  },
+  {
+    "revision": "e71a64a05392994873791dd8f8a0b999",
+    "url": "/AnthonyOkwananke/tony-softworks.github.io/static/media/me_wbg.e71a64a0.png"
+  }
+]);
